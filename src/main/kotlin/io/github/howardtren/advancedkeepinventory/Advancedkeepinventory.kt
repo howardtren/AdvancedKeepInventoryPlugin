2882b1b1d0968981a3c6be1d37470ad6cb9c2744
howard tren <howardtrengames@gmail.com>
2025-08-01 18:13:04 -0600

    first commit

---
+++ src/main/kotlin/io/github/howardtren/advancedkeepinventory/Advancedkeepinventory.kt
@@ -0,0 +1,14 @@
+package io.github.howardtren.advancedkeepinventory
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Advancedkeepinventory : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/kotlin/io/github/howardtren/advancedkeepinventory/commands/AKICommand.kt
@@ -0,0 +1,4 @@
+package io.github.howardtren.advancedkeepinventory.commands
+
+class AKICommand {
+}
